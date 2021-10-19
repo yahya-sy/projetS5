0c0cb895ec9258897f4077adc5e306349ffda11a
Speeral <lucas.proix@gmail.com>
2021-10-19 09:44:33 +0200

    pagedacceuil en "/"

--- src/Controller/PagedacceuilController.php
+++ src/Controller/PagedacceuilController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/pagedacceuil')]
+#[Route('/')]
 class PagedacceuilController extends AbstractController
 {
     #[Route('/', name: 'pagedacceuil_index', methods: ['GET'])]
