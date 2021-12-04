a493f5a2a7be94ba220232423205db5947693f74
Speeral <lucas.proix@gmail.com>
2021-12-04 23:47:39 +0100

    Paramettrage VichUploader

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Image;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -25,6 +26,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Image', 'fas fa-list', Image::class);
     }
 }
--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -6,9 +6,14 @@ use App\Repository\ImageRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Entity\File as EmbeddedFile;
 
 /**
  * @ORM\Entity(repositoryClass=ImageRepository::class)
+ * @Vich\Uploadable
  */
 class Image
 {
@@ -20,14 +25,14 @@ class Image
     private $id;
 
     /**
-         * @ORM\Column(type="string", length=255)
-         */
-        private $name;
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
 
     /**
-         * @ORM\ManyToOne(targetEntity="App\Entity\Article", inversedBy="images")
-         */
-        private $article;
+     * @ORM\ManyToOne(targetEntity="App\Entity\Article", inversedBy="images")
+     */
+    private $article;
 
     /**
      * @ORM\ManyToOne(targetEntity=Pagedacceuil::class, inversedBy="galerie")
@@ -35,14 +40,79 @@ class Image
      */
     private $pagedacceuil;
 
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="images_directory", fileNameProperty="imageName", size="imageSize")
+     *
+     * @var File|null
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string|null
+     */
+    private $imageName;
+
+    /*
+    /**
+    * @ORM\Column(type="datetime")
+    *
+    * @var \DateTimeInterface|null
+    */
+    //private $updatedAt;
+
+    /**
+     * @ORM\Column(type="blob")
+     */
+    private $File;
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable('now');
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
     }
+
     public function getName(): ?string
-        {
-            return $this->name;
-        }
+    {
+        return $this->name;
+    }
 
     public function setName(string $name): self
     {
@@ -52,9 +122,9 @@ class Image
     }
 
     public function getArticle(): ?Article
-        {
-            return $this->article;
-        }
+    {
+        return $this->article;
+    }
 
     public function setArticle(?Article $article): self
     {
@@ -71,6 +141,18 @@ class Image
     public function setPagedacceuil(?Pagedacceuil $pagedacceuil): self
     {
         $this->pagedacceuil = $pagedacceuil;
+
+        return $this;
+    }
+
+    public function getFile()
+    {
+        return $this->File;
+    }
+
+    public function setFile($File): self
+    {
+        $this->File = $File;
 
         return $this;
     }
