8f06ee0e750350f642beadfe76218f0e3d51b241
Speeral <lucas.proix@gmail.com>
2021-12-05 02:42:41 +0100

    Modif entity image

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -25,11 +25,6 @@ class Image
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $name;
-
-    /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Article", inversedBy="images")
      */
     private $article;
@@ -70,11 +65,6 @@ class Image
     private $File;
 
     /**
-     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
-     * of 'UploadedFile' is injected into this setter to trigger the update. If this
-     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
-     * must be able to accept an instance of 'File' as the bundle will inject one here
-     * during Doctrine hydration.
      *
      * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
      */
